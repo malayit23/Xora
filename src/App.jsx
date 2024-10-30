d64b8b05ba6a793c5b49f28c10b289f020d60071
Malay Chowdhury <119236564+malayit23@users.noreply.github.com>
2024-10-30 22:58:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Pricing from './components/Pricing'
+import Faq from './components/Faq'
+import Testimonials from './components/Testimonials'
+import Download from './components/Download'
+import Footer from './components/Footer'
+const App = () => {
+  return (
+    <main className='overflow-hidden'>
+      <Header/>
+      <Hero/>
+      <Features/>
+      <Pricing/>
+      <Faq/>
+      <Testimonials/>
+      <Download/>
+      <Footer/>
+    </main>
+  )
+}
+
+export default App
